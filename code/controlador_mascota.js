41ad9b143036cd510a05c63b15ea649fc604d1ae
Narizaico-code <areyes-2024435@kinal.edu.gt>
2025-06-18 23:02:58 -0600

    2. Commit 5 rama ft/AngelR

--- code/controlador_mascota.js
+++ code/controlador_mascota.js
@@ -1,1 +1,5 @@
 window.print("Hola mundo");
+console.log("Hola mascotas");
+let humano = "Hola mundo";
+let mascota = prompt("Por favor, ingresa tu mascota:");
+console.log("Hola " + mascota + " tu mascota es: " + humano);
--- code/controlador_persona.js
+++ code/controlador_persona.js
@@ -1,1 +1,2 @@
 window.print("Primer commit ft/AngelR");
+console.log(1+1/1+1);
